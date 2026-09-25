960f66de22405ecf5d58132d212b7db1dabe084b
Iryna Lienska <46135805+iramazur1991@users.noreply.github.com>
2026-09-26 00:31:01 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='towy-v2';
+const CACHE='towy-v4';
 const ASSETS=['./','./index.html','./driver.html','./manifest.json','./driver-manifest.json','./icon-192.png','./icon-512.png','./driver-icon-192.png','./driver-icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
